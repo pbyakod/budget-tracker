e5aeea7fb07b3d9ac4eaa30afa5b5de3139c96c4
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-21 11:31:42 -0700

    writing indexedDB.js function to store db transactions

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -16,3 +16,9 @@ request.onsuccess = function(event) {
 request.onerror = function(event) {
     console.log("ERROR!" + event.target.errorCode);
 };
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+}
