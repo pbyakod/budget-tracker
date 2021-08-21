8fd8f3dadfd2ee92ef34f44bac760dca9ce238c1
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-21 11:12:14 -0700

    creating service-worker file with cache to existing files

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1"; 
